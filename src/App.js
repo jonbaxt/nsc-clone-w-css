2dfce31d5565bd8830bc209e6bb7357040f1842d
jonbaxt <jonbaxt@gmail.com>
2018-08-18 05:36:47 -0600

    Built rest of page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Industry_Section from './components/Industry_Section/Industry_Section';
 import Career_Section from './components/Career_Section/Career_Section';
 import Education from './components/Education/Education';
 import Events from './components/Events/Events';
+import News from './components/News/News';
 
 import './App.css';
 
 import ship1 from './resources/fill-4.svg';
 import ship2 from './resources/fill-10.svg';
 import ship3 from './resources/fill-1.svg';
+import logo from './resources/nsc-logo.svg';
 
 class App extends Component {
   render() {
@@ -38,6 +40,26 @@ class App extends Component {
       <Career_Section />
       <Education />
       <Events />
+      <News />
+        <div className='line'></div>
+      <footer>
+
+        <img src={logo} alt= '' />
+        <div>
+        <a>ABOUT</a>
+        <a>INDUSTRY</a>
+        <a>CAREERS</a>
+        <a>EDUCATION & TRAINING</a>
+        </div>
+        <div>
+        <a>EVENTS</a>
+        <a>NEWS</a>
+        <a>CONTACT</a>
+        </div>
+        <div>
+        <a>CONNECT WITH US</a>
+        </div>
+      </footer>
   </div>
     );
   }
